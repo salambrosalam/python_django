9a77c97612ef06aee3b49baee50ef527faebe100
salambrosalam <romashkin1273@gmail.com>
2021-08-10 16:47:57 +0200

    model creating process

--- start_project/web_app/news/models.py
+++ start_project/web_app/news/models.py
@@ -2,3 +2,9 @@ from django.db import models
 
 # Create your models here.
 
+class News(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
