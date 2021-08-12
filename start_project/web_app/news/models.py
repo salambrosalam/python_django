f880b997c35d7960cf18babab71807f5d0590567
salambrosalam <romashkin1273@gmail.com>
2021-08-12 16:47:38 +0200

    CRUD noticed, model modified, some data added to DB

--- start_project/web_app/news/models.py
+++ start_project/web_app/news/models.py
@@ -7,4 +7,8 @@ class News(models.Model):
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
