bc2fdc3c26483f462db91f901fcb2bf1dc13fe7d
salambrosalam <romashkin1273@gmail.com>
2021-08-25 16:42:01 +0200

    Admin panel started

--- start_project/web_app/news/models.py
+++ start_project/web_app/news/models.py
@@ -3,12 +3,30 @@ from django.db import models
 # Create your models here.
 
 class News(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    title = models.CharField(max_length=100, verbose_name="Название")
+    content = models.TextField(blank=True, verbose_name="Контент")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата публикации")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Обновлено")
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Картинка", blank=True)
     is_published = models.BooleanField(default=True)
+    category = models.ForeignKey("Category", on_delete=models.PROTECT, null=True, verbose_name="Название категории")
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = "Новость"
+        verbose_name_plural = "Новости"
+        ordering = ["-created_at"]
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name="Название категории")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["title"]
--- start_project/web_app/news/views.py
+++ start_project/web_app/news/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import News
+
 # Create your views here.
 
 def index(request):
     print(request)
-    return HttpResponse('Hello World')
+    news = News.objects.all()
+    return render(request, "news/index.html", {"news": news, "title": "Список новостей"})
 
 def test(request):
     print("salam")
-    return HttpResponse("<H1>TEST PAGE</H1>")
+    return HttpResponse("<H1>TEST PAGE</H1> ")
