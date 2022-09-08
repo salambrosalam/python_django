310c397e1d60af885cf020e4a907ea85837fff82
salambrosalam <romashkin1273@gmail.com>
2022-09-08 20:43:04 +0200

    fixing category names

--- start_project/web_app/web_app/settings.py
+++ start_project/web_app/web_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r&zg(ms#+t3rmftwynq5*bw65w$g)gtg$61j15zhbjxdw*g$jt'
+SECRET_KEY = 'SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
